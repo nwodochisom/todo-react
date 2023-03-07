1cf3e5f8daed3f227752f9c48b7cab539bba2853
Michael Wejinya <wejinyamichael@gmail.com>
2023-03-07 09:58:29 -0800

    add navbar and style css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Main from './Main';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Nav />
       <Main />
     </div>
   );
